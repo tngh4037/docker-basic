c49302d5bb2be60637c98b1140267b94ad427a19
tngh4037 <tngh4037@gmail.com>
2025-05-03 05:59:07 +0900

    study: Spring Boot, MySQL 컨테이너 동시에 띄워보기

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:17-jdk
 
 WORKDIR /app/
 
-COPY ./build/libs/*SNAPSHOT.jar app.jar
+COPY ./build/libs/*-SNAPSHOT.jar app.jar
 
 EXPOSE 8080
 
--- src/test/java/com/example/docker/DockerApplicationTests.java
+++ src/test/java/com/example/docker/DockerApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.docker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 class DockerApplicationTests {
 
